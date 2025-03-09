1838948a0cdb92d8e96d07824f4b76fc67e28da9
theclassworks <2300@mail.ir>
2025-03-09 20:27:30 +0330

    compidan

--- cw-slider js-simple/assets/js/main.js
+++ cw-slider js-simple/assets/js/main.js
@@ -1,16 +1,17 @@
 let Data = [
-  { id: 0, src: "assets/img/header-1.jpg" },
-  { id: 1, src: "assets/img/header-2.jpg" },
-  { id: 2, src: "assets/img/header-3.jpg" },
-  { id: 3, src: "assets/img/header-4.jpg" },
+  { id: 0, src: "assets/img/header-1.jpg", titr: "آموزش وب" },
+  { id: 1, src: "assets/img/header-2.jpg", titr: "آموزش هوش" },
+  { id: 2, src: "assets/img/header-3.jpg", titr: "" },
+  { id: 3, src: "assets/img/header-4.jpg", titr: "" },
 ];
 let ax = document.querySelector(".slider-img");
 let forwardbtn = document.querySelector(".slider-forward-btn");
 let backbtn = document.querySelector(".slider-back-btn");
+let titr = document.getElementById("titr");
 
 // console.log(Data[1]);
 
-setInterval(nextpic, 3000);
+let slideshow = setInterval(nextpic, 3000);
 
 let index = 0;
 function nextpic() {
@@ -20,4 +21,21 @@ function nextpic() {
   }
   console.log(Data[index].src);
   ax.setAttribute("src", Data[index].src);
+  titr.innerText = Data[index].titr;
+  clearInterval(slideshow);
+  slideshow = setInterval(nextpic, 3000);
+}
+
+forwardbtn.addEventListener("click", nextpic);
+backbtn.addEventListener("click", prevpic);
+
+function prevpic() {
+  index--;
+  if (index < 0) {
+    index = Data.length - 1;
+  }
+  console.log(Data[index].src);
+  ax.setAttribute("src", Data[index].src);
+  clearInterval(slideshow);
+  slideshow = setInterval(nextpic, 3000);
 }
